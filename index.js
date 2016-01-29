25982383381281b7525566f04d2d61f663f684e2
Alex Bennett <alexandergraybennett@gmail.com>
2016-01-29 11:56:01 -0700

    adding more stuff

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
-var books = require('./routes/books');
-var authors = require('./routes/authors');
-var getbooks = require('./routes/getbooks');
+var marketing = require('./routes/marketing');
 var app = express();
 var cors = require('cors');
-var knex = require('knex')({
-  client: 'pg',
-  connection: 'postgres://wptugffjhbpqta:V8lvqBLY6Ry12tRP0GW7mQ87Zj@ec2-54-83-40-119.compute-1.amazonaws.com:5432/d6b88ef20b57to?ssl=true'
-});
 // use modules
 app.set('view engine', 'html');
 app.use(express.static('./views'));
@@ -21,15 +14,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
+
 app.use(logger('dev'));
 app.use(cookieParser());
+
 app.use('/', routes);
 app.use('/marketing', marketing);
 
 // radnom
-app.get('/edit', function(req,res,next){
-  res.redirect('/updatebook.html')
-})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -55,4 +47,8 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
+app.listen(3030, function(){
+  console.log('vhats up')
+})
 module.exports = app;
--- knexfile.js
+++ knexfile.js
@@ -4,8 +4,8 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: "postgres://kkavxscrkrhght:LDRokhc3yTLzuzNH-1yxoIJIvU@ec2-54-225-195-249.compute-1.amazonaws.com:5432/db4emn8nb1aepe?ssl=true",
-  // connection: "postgres://localhost/sandy"
+  //   connection: "postgres://kkavxscrkrhght:LDRokhc3yTLzuzNH-1yxoIJIvU@ec2-54-225-195-249.compute-1.amazonaws.com:5432/db4emn8nb1aepe?ssl=true",
+  connection: "postgres://localhost/sandy"
 
   },
 
